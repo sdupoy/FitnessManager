47b7e7c1fc99c3e61a882e9741e71aeebfced6f0
lisiling <sli116@hawk.iit.edu>
2016-04-18 17:46:59 -0500

    fixed commit with sign up and sgin in

---
+++ app/src/main/java/edu/iit/sat/itmd555/fp/fitnessmanager/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.mypackage.fitness;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    Button btnSignIn;
+    Button btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button) findViewById(R.id.btnSingIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent i = null;
+        switch(v.getId()){
+            case R.id.btnSingIn:
+                i = new Intent(this,SignInActivity.class);
+                break;
+            case R.id.btnSignUp:
+                i = new Intent(this,SignUpActivity.class);
+                break;
+        }
+        startActivity(i);
+    }
+
+
+
+}
+
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
---
+++ app/src/main/java/edu/iit/sat/itmd555/fp/fitnessmanager/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.mypackage.fitness;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_screen);
+    }
+
+
+
+}
