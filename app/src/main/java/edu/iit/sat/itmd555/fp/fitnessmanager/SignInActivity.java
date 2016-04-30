782899d4c47958fe23aac91ecb453753857c81bc
lisiling <sli116@hawk.iit.edu>
2016-04-30 16:38:02 -0500

    add remember me checkbox on sign in page

--- app/src/main/java/edu/iit/sat/itmd555/fp/fitnessmanager/SignInActivity.java
+++ app/src/main/java/edu/iit/sat/itmd555/fp/fitnessmanager/SignInActivity.java
@@ -1,36 +1,31 @@
 package edu.iit.sat.itmd555.fp.fitnessmanager;
 
 import android.app.Activity;
-import android.app.backup.SharedPreferencesBackupHelper;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Objects;
 
 public class SignInActivity extends Activity {
+
+    private TextView link_signup;
     private SqlHelper db;
     private EditText Email, password;
-    private CheckBox rmPass;
+    private CheckBox rememberPass;
     private Button btnSignIn;
-    private String EmailValue,passwordValue;
+    private String EmailValue, passwordValue;
     private SharedPreferences sp;
-    private TextView link_signup;
+    private SharedPreferences.Editor spEditor;
+    private Boolean saveLogin;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,21 +34,67 @@ public class SignInActivity extends Activity {
         db = SqlHelper.getInstance(getApplicationContext());
 
         //get objects
-        sp = this.getSharedPreferences("userInfo", Context.MODE_WORLD_READABLE);
+        sp = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        spEditor = sp.edit();
+        rememberPass = (CheckBox) findViewById(R.id.rmPass);
         Email = (EditText) findViewById(R.id.etEmail);
         password = (EditText) findViewById(R.id.etPass);
-        rmPass = (CheckBox) findViewById(R.id.rmPass);
         btnSignIn = (Button) findViewById(R.id.btnSignIn);
         link_signup = (TextView) findViewById(R.id.link_signup);
 
 
-        //check status of remember password's checkbox
-//        if(sp.getBoolean("ISCHECK", false))
-//        {
-//            //设置默认是记录密码状态
-//            rmPass.setChecked(true);
-//            Email.setText(sp.getString("Email", ""));
-//            password.setText(sp.getString("PASSWORD", ""));
+        saveLogin = sp.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            Email.setText(sp.getString("Email", ""));
+            password.setText(sp.getString("password", ""));
+            rememberPass.setChecked(true);
+        }
+
+            // set OnclickListener  useremail：li@123.com password：123
+        btnSignIn.setOnClickListener(new OnClickListener() {
+
+                public void onClick(View v) {
+                    EmailValue = Email.getText().toString();
+                    passwordValue = password.getText().toString();
+                    if (Email.getText().length() != 0 || password.getText().length() != 0) {
+                        if (db.getUserByEmail(EmailValue) != null) {
+                            if (Objects.equals(db.getUserByEmail(EmailValue).getPassword(), passwordValue)) {
+                                Toast.makeText(SignInActivity.this, "SignIn successful!", Toast.LENGTH_SHORT).show();
+
+                                //until successfully sign in and check box is clicked ----then save users information
+                                if (rememberPass.isChecked()) {
+                                    //remember password and login email
+                                    spEditor.putBoolean("saveLogin", true);
+                                    spEditor.putString("Email", EmailValue);
+                                    spEditor.putString("password", db.getUserByEmail(EmailValue).getPassword());
+                                    spEditor.commit();
+                                }else{
+                                    spEditor.clear();
+                                    spEditor.commit();
+                                }
+                                //switch page
+                                Intent i = new Intent(getApplicationContext(), MainTabActivity.class);
+                                startActivity(i);
+                            } else if (EmailValue.equals("li@123.com") && passwordValue.equals("123")) {
+                                Toast.makeText(SignInActivity.this, "SignIn successful!", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(getApplicationContext(), MainTabActivity.class);
+                                startActivity(i);
+                                //finish();
+
+                            } else {
+
+                                Toast.makeText(SignInActivity.this, "Email or Password wrong! Please try again!", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+
+                    } else {
+
+                        Toast.makeText(SignInActivity.this, "Email or Password empty! Please try again!", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+        });
 
         //set OnclickListener for move to signup page
         link_signup.setOnClickListener(new OnClickListener() {
@@ -63,68 +104,7 @@ public class SignInActivity extends Activity {
                 startActivity(i);
             }
         });
-
-        // set OnclickListener  useremail：li@123.com password：123
-        btnSignIn.setOnClickListener(new OnClickListener() {
-
-            public void onClick(View v) {
-                EmailValue = Email.getText().toString();
-                passwordValue = password.getText().toString();
-                if(Email.getText().length()!=0 || password.getText().length()!=0){
-                    if(db.getUserByEmail(EmailValue)!=null){
-                        if(Objects.equals(db.getUserByEmail(EmailValue).getPassword(), passwordValue)){
-                            Toast.makeText(SignInActivity.this,"SignIn successful!", Toast.LENGTH_SHORT).show();
-                            //登录成功和记住密码框为选中状态才保存用户信息
-//                    if(rmPass.isChecked())
-//                    {
-//                        //记住用户名、密码、
-//                        Editor editor = sp.edit();
-//                        editor.putString("Email", EmailValue);
-//                        editor.putString("PASSWORD",passwordValue);
-//                        editor.commit();
-//                    }
-                            //跳转界面
-
-                            Intent i = new Intent(getApplicationContext(), MainTabActivity.class);
-                            startActivity(i);
-                        } else if(EmailValue.equals("li@123.com")&&passwordValue.equals("123")){
-                            Toast.makeText(SignInActivity.this,"SignIn successful!", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(getApplicationContext(), MainTabActivity.class);
-                            startActivity(i);
-                            //finish();
-
-                        }else{
-
-                            Toast.makeText(SignInActivity.this,"Email or Password wrong! Please try again!", Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-
-                }  else{
-
-                    Toast.makeText(SignInActivity.this,"Email or Password empty! Please try again!", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
-
-        //监听记住密码多选框按钮事件
-//        rmPass.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-//            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
-//                if (rmPass.isChecked()) {
-//
-//                    System.out.println("checked remember password");
-//                    sp.edit().putBoolean("ISCHECK", true).commit();
-//
-//                }else {
-//
-//                    System.out.println("unchecked remember password");
-//                    sp.edit().putBoolean("ISCHECK", false).commit();
-//
-//                }
-//
-//            }
-//        });
     }
 }
+
 
